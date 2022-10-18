2f1c01f8f72a8b2a44a0b82be0e4607a8f99d6a1
chris-watson <cwatson@Christophers-MacBook-Pro.local>
2022-10-18 19:22:27 -0400

    inital setup / add mocking

---
+++ scripts/dockerize_mock_api.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+mockoon-cli dockerize --data api/testing/mocks/person.json --port 31006  --output api/testing/mocks/person_dockerfile
+
---
+++ scripts/mock_api_build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd mocks
+docker build -t mock-person-api -f person_dockerfile
